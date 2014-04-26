73c863981a23268505588c23253b8ac91074fff2
dimonpvt <dimonpvt1236@yandex.ru>
2014-04-26 20:15:14 +0400

    added operation INV

--- src/evolalg/EvolAlg.java
+++ src/evolalg/EvolAlg.java
@@ -24,12 +24,22 @@ public class EvolAlg {
         //in: chromo length
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         String number1 = null;
-        try {
+       /* try {
             number1 = in.readLine();
         } catch (IOException ioex) {
             System.out.println(ioex.toString());
         }
-        Population p = new Population();
+        */
+        Chromosome xrom1 = new Chromosome(10);
+        //Chromosome xrom2 = new Chromosome(10);
+        xrom1.setData("0110101001");
+        //xrom2.setData("1001011010");
+        int crossPoint[];
+        crossPoint = xrom1.INV_twoPoint();
+
+        System.out.println(xrom1.toString(crossPoint));
+        //System.out.println(xrom2.toString(crossPoint));
+      /*  Population p = new Population();
         p.setChromoLength(Integer.valueOf(number1));
 
         p.genFullPopulation(Integer.valueOf(number1));
@@ -44,7 +54,7 @@ public class EvolAlg {
         p.genFullPopulation(Integer.valueOf(number1));
         p.addShotgun(50f);
         System.out.println(p.toString());
-        System.out.println("===========");
+        System.out.println("===========");*/
        
     }
 
